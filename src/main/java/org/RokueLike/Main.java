0813aed6fd583f014c9e367eac05c82fe1273793
earica21 <earica21@ku.edu.tr>
2024-12-19 19:48:35 +0300

    buildrender ve playrender mergelendi

--- src/main/java/org/RokueLike/Main.java
+++ src/main/java/org/RokueLike/Main.java
@@ -3,8 +3,6 @@ package org.RokueLike;
 import org.RokueLike.ui.Textures;
 import org.RokueLike.ui.Window;
 
-
-
 public class Main {
 
     public static void main(String[] args) {
@@ -21,3 +19,4 @@ public class Main {
     }
 
 }
+
