58be1e3f410b9a82faffc54a1d020dca29eab490
ErayOnder <erayonder21@ku.edu.tr>
2025-01-30 22:04:53 +0300

    fixed a comment

--- src/main/java/org/RokueLike/Main.java
+++ src/main/java/org/RokueLike/Main.java
@@ -7,10 +7,6 @@ import java.util.Locale;
 
 public class Main {
 
-    // Note: There are MessageBox instance in our code which notifies the user on relevant game states.
-    // However, there are sometimes delays on these messages due to the hardware issues of the computer that our program is executed.
-    // We will come up with a better solution in the final submission deadline.
-
     public static void main(String[] args) {
         // Set the default locale to English to ensure there are no problems extracting the Textures
         Locale.setDefault(Locale.ENGLISH);
